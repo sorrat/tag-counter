1626c86129b216f66426b4ac9709e8001632b0ae
Fedor Stepanov <yasorrat@gmail.com>
2016-06-21 17:07:48 +0400

    Cleanup represent.py

--- tag_counter/represent.py
+++ tag_counter/represent.py
@@ -8,18 +8,14 @@ def print_tag_counts(url):
     print format_tag_counts(counts)
 
 
-def second(items):
-    return items[1]
-
-
-def reverse_by_second(counts):
-    return sorted(counts, key=lambda array: int(array[1]), reverse=True)
+def desc_by_count(counts):
+    _count = lambda x: int(x[1])
+    return sorted(counts, key=_count, reverse=True)
 
 
 def format_tag_counts(counts):
-    second = lambda items: items[0]
     max_tag_length = max(len(x[0]) for x in counts) + 1
 
     return '\n'.join(
         '{0:{2}} {1}'.format(tag, count, max_tag_length)
-        for tag, count in reverse_by_second(counts))
+        for tag, count in desc_by_count(counts))
